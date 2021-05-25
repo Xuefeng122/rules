6f015516c0892a872b12cf6fd8944ca82a950c0a
Xuefeng122 <62353673+Xuefeng122@users.noreply.github.com>
2021-05-25 23:45:33 +0800

    Update dy.js

--- dy.js
+++ dy.js
@@ -1,5 +1,5 @@
 hostname = api5-core-c-lf.amemv.com, api3-core-c-lf.amemv.com
 
-^https?:\/\/.+?\.amemv\.com\/aweme\/v\d\/(feed|aweme\/post|follow\/feed|nearby\/feed|search\/item|general\/search\/single|hot\/search\/video\/list)\/ url script-request-header https://raw.githubusercontent.com/zwf234/rules/master/douyin.js
-^https?:\/\/.+?\.amemv\.com\/aweme\/v\d\/(feed|aweme\/post|follow\/feed|nearby\/feed|search\/item|general\/search\/single|hot\/search\/video\/list)\/ url script-response-body https://raw.githubusercontent.com/zwf234/rules/master/douyin.js
+^https?:\/\/.+?\.amemv\.com\/aweme\/v\d\/(feed|aweme\/post|follow\/feed|nearby\/feed|search\/item|general\/search\/single|hot\/search\/video\/list)\/ url script-request-header https://raw.githubusercontent.com/xuefeng122/rules/master/douyin.js
+^https?:\/\/.+?\.amemv\.com\/aweme\/v\d\/(feed|aweme\/post|follow\/feed|nearby\/feed|search\/item|general\/search\/single|hot\/search\/video\/list)\/ url script-response-body https://raw.githubusercontent.com/xuefeng122/rules/master/douyin.js
 
